7f3f92756e22be05c6c9258cfde025623e222951
a240888 <it30116@users.noreply.github.com>
2020-11-09 22:03:40 +0800

    11/9
    pp系統完善
    道具系統完善
    顯示原先血量

--- 賽爾號/賽爾號.py
+++ 賽爾號/賽爾號.py
@@ -1,3 +1,5 @@
+import random
+import os
 class switch(object):
     def __init__(self, value):
         self.value = value
@@ -17,7 +19,7 @@ class switch(object):
             return True
         else:
             return False
-import random
+
 
 def phydmg(power,atk,defense,counter):
     """物攻傷害"""
@@ -164,8 +166,130 @@ def 判斷pp(me):
     elif me==6 and skill=='4':
         if 雷伊pp[3]>0:
             return True
+def 判斷血量是否超過(me):
+    if me==1:
+        if 哈莫雷特[0]>原哈莫雷特[0]:
+            哈莫雷特[0]=原哈莫雷特[0]
+    elif me==2:
+        if 羅特利斯[0]>原羅特利斯[0]:
+            羅特利斯[0]=原羅特利斯[0]
+    elif me==3:
+        if 魔焰猩猩[0]>原魔焰猩猩[0]:
+            魔焰猩猩[0]=原魔焰猩猩[0]
+    elif me==4:
+        if 薩帕克[0]>原薩帕克[0]:
+            薩帕克[0]=原薩帕克[0]
+    elif me==5:
+        if 卡利斯[0]>原卡利斯[0]:
+            卡利斯[0]=原卡利斯[0]
+    elif me==6:
+        if 雷伊[0]>原雷伊[0]:
+            雷伊[0]=原雷伊[0]
+def 判斷pp是否超過(me):
+    if me==1:
+        for i in range(4):
+            if 哈莫雷特pp[i]>原哈莫雷特pp[i]:
+                哈莫雷特pp[i]=原哈莫雷特pp[i]
+    elif me==2:
+        for i in range(4):
+            if 羅特利斯pp[i]>原羅特利斯pp[i]:
+                羅特利斯pp[i]=原羅特利斯pp[i]
+    elif me==3:
+        for i in range(4):
+            if 魔焰猩猩pp[i]>原魔焰猩猩pp[i]:
+                魔焰猩猩pp[i]=原魔焰猩猩pp[i]
+    elif me==4:
+        for i in range(4):
+            if 薩帕克pp[i]>原薩帕克pp[i]:
+                薩帕克pp[i]=原薩帕克pp[i]
+    elif me==5:
+        for i in range(4):
+            if 卡利斯pp[i]>原卡利斯pp[i]:
+                卡利斯pp[i]=原卡利斯pp[i]
+    elif me==6:
+        for i in range(4):
+            if 雷伊pp[i]>原雷伊pp[i]:
+                雷伊pp[i]=原雷伊pp[i]
 
-             
+
+def pp變化(me,which):
+    global 哈莫雷特pp;global 羅特利斯pp;global 魔焰猩猩pp;global 薩帕克pp;global 卡利斯pp;global 雷伊pp
+    global increase
+    if me==1 and (which=='3' or which=='4'or which=='5'):
+        for i in range(4):
+            哈莫雷特pp[i]+=increase
+    elif me==2 and (which=='3' or which=='4'or which=='5'):
+        for i in range(4):
+            羅特利斯pp[i]+=increase
+    elif me==3 and (which=='3' or which=='4'or which=='5'):
+        for i in range(4):
+            魔焰猩猩pp[i]+=increase
+    elif me==4 and (which=='3' or which=='4'or which=='5'):
+        for i in range(4):
+            薩帕克pp[i]+=increase
+    elif me==5 and (which=='3' or which=='4'or which=='5'):
+        for i in range(4):
+            卡利斯pp[i]+=increase
+    elif me==6 and (which=='3' or which=='4'or which=='5'):
+        for i in range(4):
+            雷伊pp[i]+=increase
+def 血量變化(me,which):
+    global 哈莫雷特;global 羅特利斯;global 魔焰猩猩;global 薩帕克;global 卡利斯;global 雷伊
+    global increase
+    if me==1 and (which=='1' or which=='2'):
+        哈莫雷特[0]+=increase
+    elif me==2 and (which=='1' or which=='2'):
+        羅特利斯[0]+=increase
+    elif me==3 and (which=='1' or which=='2'):
+        魔焰猩猩[0]+=increase
+    elif me==4 and (which=='1' or which=='2'):
+        薩帕克[0]+=increase
+    elif me==5 and (which=='1' or which=='2'):
+        卡利斯[0]+=increase
+    elif me==6 and (which=='1' or which=='2'):
+        雷伊[0]+=increase
+def 道具(which):
+    global 道具數量;global namepp;global increase
+    if which=='1' and 道具數量[0]>0:
+        if namepp==True:
+            print('使用了高級體力藥劑,回復150生命')
+            道具數量[0]-=1
+        namepp=False
+        increase=150
+    elif which=='2' and 道具數量[1]>0:
+        if namepp==True:
+            print('使用了究集體力藥劑,回復200生命')
+            道具數量[1]-=1
+        namepp=False
+        increase=200
+    elif which=='3' and 道具數量[2]>0:
+        if namepp==True:
+            print('使用了初級活力藥劑,PP增加了5')
+            道具數量[2]-=1
+        namepp=False
+        increase=5
+    elif which=='4' and 道具數量[3]>0:
+        if namepp==True:
+            print('使用了中級活力藥劑,PP增加了10')
+            道具數量[3]-=1
+        namepp=False
+        increase=10
+    elif which=='5' and 道具數量[4]>0:
+        if namepp==True:
+            print('使用了高級活力藥劑,PP增加了15')
+            道具數量[4]-=1
+        namepp=False
+        increase=15
+    elif namepp==True:
+         print('道具數量不夠!')
+
+         increase=0
+         玩家攻擊()
+    else :
+        increase=0
+
+
+
 
 
 def 技能():
@@ -402,17 +526,17 @@ def 技能威力(skill):
                if a('2'):
                     mypower=140
                     skilltype=雷伊pp[5]
-                    雷伊pp[0]-=1
+                    雷伊pp[1]-=1
                     break
                if a('3'):
                     mypower=150
                     skilltype=雷伊pp[6]
-                    雷伊pp[0]-=1
+                    雷伊pp[2]-=1
                     break
                if a('4'):
                     mypower=0
                     skilltype=雷伊pp[7]
-                    雷伊pp[0]-=1
+                    雷伊pp[3]-=1
                     break
                if True:
                     print('輸入錯誤')
@@ -434,8 +558,29 @@ def 我方誰在場上(me):
     if me ==6:
         return re
 
-
+def 原我方血量(me):
+     global 原哈莫雷特;global 原羅特利斯;global 原魔焰猩猩;global 原薩帕克;global 原卡利斯;global 原雷伊
+     for case in switch(me):
+        if case(int(1)):
+            return 原哈莫雷特[0]
+            break
+        if case(int(2)):
+            return 原羅特利斯[0]
+            break
+        if case(int(3)):
+            return 原魔焰猩猩[0]
+            break
+        if case(int(4)):
+            return 原薩帕克[0]
+            break
+        if case(int(5)):
+            return 原卡利斯[0]
+            break
+        if case(int(6)):
+            return 原雷伊[0]
+            break
 def 我方血量(me):
+     global 哈莫雷特;global 羅特利斯;global 魔焰猩猩;global 薩帕克;global 卡利斯;global 雷伊
      for case in switch(me):
         if case(int(1)):
             return 哈莫雷特[0]
@@ -454,6 +599,29 @@ def 我方血量(me):
             break
         if case(int(6)):
             return 雷伊[0]
+            break
+def 原敵方血量(you):
+     for case in switch(you):
+        if case(int(1)):
+            return 原boss1[0]
+            break
+        if case(int(2)):
+            return 原boss2[0]
+            break
+        if case(int(3)):
+            return 原boss3[0]
+            break
+        if case(int(4)):
+            return 原boss4[0]
+            break
+        if case(int(5)):
+            return 原boss5[0]
+            break
+        if case(int(6)):
+            return 原boss6[0]
+            break
+        if True:
+            print('bug')
             break
 def 敵方血量(you):
      for case in switch(you):
@@ -502,27 +670,33 @@ def 能力提升():
     global skilltype;global hitrate;global me
     global skill; global mypower;global haskilllv;global loskilllv;global moskilllv;global saskilllv;global reskilllv;global caskilllv
     if me==1 and skill=='1':
-                haskilllv+=0.5
+                if haskilllv<4:
+                    haskilllv+=0.5
                 哈莫雷特[1]=int(原哈莫雷特[1]*haskilllv);哈莫雷特[2]=int(原哈莫雷特[2]*haskilllv);哈莫雷特[3]=int(原哈莫雷特[3]*haskilllv);哈莫雷特[4]=int(原哈莫雷特[4]*haskilllv)
                 print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',攻擊、防禦、特攻、特防提高了')
     elif  me==2 and skill=='2':
-                loskilllv+=0.5
+                if loskilllv<4:
+                    loskilllv+=0.5
                 羅特利斯[3]=int(原哈莫雷特[3]*loskilllv)
                 print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),'特攻大幅提高了')
-    elif me==3 and skill=='2':
-                moskilllv+=1
+    elif me==3 and skill=='1':
+                if moskilllv<4:
+                    moskilllv+=1
                 魔焰猩猩[1]=int(原魔焰猩猩[1]*moskilllv);魔焰猩猩[3]=int(原魔焰猩猩[3]*moskilllv)
                 print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',物攻、特攻大幅提高了')
     elif me==4 and skill=='2':
-                saskilllv+=1
+                if saskilllv<4:
+                    saskilllv+=1
                 薩帕克[3]=int(原薩帕克[3]*saskilllv)
                 print(我方誰在場上(me),'用了',我方技能名稱(me,skill),',特攻大幅提高了')
     elif me==5 and skill=='3':
-                caskilllv+=0.5
+                if caskilllv<4:
+                    caskilllv+=0.5
                 卡利斯[1]=int(原卡利斯[1]*caskilllv);卡利斯[2]=int(原卡利斯[2]*caskilllv)
                 print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',攻擊、防禦提高了')
     elif me==6 and skill=='4':
-                reskilllv+=0.5
+                if reskilllv:
+                    reskilllv+=0.5
                 雷伊[2]=int(雷伊[2]*reskilllv);雷伊[4]=int(雷伊[4]*reskilllv)
                 print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',防禦、特防提高了')
 def 特殊狀態(me):
@@ -724,8 +898,12 @@ def 判斷效果(you):
             elif me==3 and skill=='4':
                 特殊狀態(me)
             elif me==3 and skill=='2':
-                print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
-                boss3[0]-=判斷傷害(me)
+                if random.randint(1,100)<=5:
+                    print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',boss3[0],'傷害')
+                    boss3[0]=0
+                else:
+                    print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
+                    boss3[0]-=判斷傷害(me)
             elif me==3 and skill=='3':
                 print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
                 boss3[0]-=判斷傷害(me)
@@ -791,8 +969,12 @@ def 判斷效果(you):
             elif me==3 and skill=='4':
                 特殊狀態(me)
             elif me==3 and skill=='2':
-                print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
-                boss4[0]-=判斷傷害(me)
+                if random.randint(1,100)<=5:
+                    print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',boss3[0],'傷害')
+                    boss3[0]=0
+                else:
+                    print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
+                    boss3[0]-=判斷傷害(me)
             elif me==3 and skill=='3':
                 print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
                 boss4[0]-=判斷傷害(me)
@@ -882,8 +1064,12 @@ def 判斷效果(you):
             elif me==3 and skill=='1':
                 能力提升()
             elif me==3 and skill=='2':
-                print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
-                boss6[0]-=判斷傷害(me)
+                if random.randint(1,100)<=5:
+                    print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',boss3[0],'傷害')
+                    boss3[0]=0
+                else:
+                    print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
+                    boss3[0]-=判斷傷害(me)
             elif me==3 and skill=='3':
                 print(我方誰在場上(me),'使用了',我方技能名稱(me,skill),',造成了',判斷傷害(me),'傷害')
                 boss6[0]-=判斷傷害(me)
@@ -924,8 +1110,11 @@ def 玩家攻擊():
     global skill
     global me
     global skilltype
-    print('我方血量:',我方血量(me))
-    print('敵方血量:',敵方血量(you))
+    global name
+    global namepp
+    os.system('clear')
+    print('我方血量:',我方血量(me),'/',原我方血量(me),sep='')
+    print('敵方血量:',敵方血量(you),'/',原敵方血量(you),sep='')
     print('輪到你進攻了')
     print('1:替換 2:攻擊 3:顯示狀態 4:技能說明 5:使用道具')
     now=str(input())
@@ -981,6 +1170,9 @@ def 玩家攻擊():
           技能威力(skill)
           判斷效果(you)
           譜尼攻擊()
+        elif skill!='1' and skill!='2' and skill!='3' and skill!='4':
+            print('輸入錯誤')
+            玩家攻擊()
         else:
             print('pp不足')
             玩家攻擊()
@@ -991,10 +1183,17 @@ def 玩家攻擊():
     elif now=='4':
         技能說明()
         玩家攻擊()
+    elif now=='5':
+        print('要使用甚麼道具:1:高級體力藥劑',道具數量[0],'/5  2:究極體力藥劑',道具數量[1],'/5  3:初級活力藥劑',道具數量[2],'/15  4:中級活力藥劑',道具數量[3],'/15  5:高級活力藥劑',道具數量[4],'/5',sep='')
+        which=input()
+        道具(which)
+        血量變化(me,which)
+        pp變化(me,which)
+        namepp=True
+        判斷pp是否超過(me)
+        判斷血量是否超過(me)
+        譜尼攻擊()
 
-
-
-        
         
     elif True:
         print('輸入錯誤')
@@ -1022,24 +1221,28 @@ global skill
 global skilltype
 global haskilllv;global loskilllv;global moskilllv;global saskilllv;global caskilllv;global reskilllv
 global hitrate;global stop;global fire;global 寄生;
+global 哈莫雷特;global 羅特利斯;global 魔焰猩猩;global 薩帕克;global 卡利斯;global 雷伊
+global 道具數量;global namepp
+
 me=1
-you=1
+you=3
 haskilllv=loskilllv=moskilllv=saskilllv=caskilllv=reskilllv=1
 hitrate=1
 stop=0
-
+namepp=True
 哈莫雷特=[402,302,225,202,215,0.5,2];原哈莫雷特=[402,302,225,202,215,0.5,2] 
 羅特利斯=[342,236,191,234,197,1.5,0.75];原羅特利斯=[342,236,191,234,197,1.5,0.75]
 魔焰猩猩=[378,282,193,231,193,2,0.5];原魔焰猩猩=[378,282,193,231,193,2,0.5]
 薩帕克=[358,232,191,234,235,0.5,2];原薩帕克=[358,232,191,234,235,0.5,2]
 卡利斯=[308,256,233,180,201,2,0.5];原卡利斯=[308,256,233,180,201,2,0.5]
-雷伊=[343,229,206,221,210,1,1];原雷伊=[343,229,206,221,210,1,1]
+雷伊=[343,229,206,221,210,1,1];原雷伊=[343,229,206,221,210,2,0.5]
 hskill=[0,'龍之意志','迴避','龍王波','龍王滅碎陣']
 lskill=[0,'幻化之火','火之意志','灼燒','天火鳳凰']
 mskill=[0,'覺醒','絕命火焰','全力一擊','火焰漩渦']
 sskill=[0,'相位移動','精神強化','樂鏡迷蹤','淨化街']
 cskill=[0,'寄生種子','催眠粉','硬化','疾風快刀']
 rskill=[0,'雷神天明閃','元氣電光球','瞬雷天閃','閃電鬥氣']
+bossskill=[0,'旋滅裂空陣','千烈虛光閃','聖光氣']
 ha='哈莫雷特'
 lo='羅特利斯'
 mo='魔焰猩猩'
@@ -1052,18 +1255,26 @@ b3='譜尼三血'
 b4='譜尼四血'
 b5='譜尼五血'
 b6='譜尼六血'
-boss1=[7000,281,236,248,236]
-boss2=[8000,281,236,248,236]
-boss3=[9000,281,236,248,236]
-boss4=[10000,281,236,248,236]
-boss5=[20000,281,236,248,236]
-boss6=[65000,281,236,248,236]
-哈莫雷特pp=[10,15,15,5,3,4,1,1]
-羅特利斯pp=[20,15,25,5,0,3,0,2]
-魔焰猩猩pp=[10,10,5,15,3,1,1,2]
-薩帕克pp=[20,20,5,30,4,3,2,3]
-卡利斯pp=[10,15,20,10,0,0,3,1]
-雷伊pp=[3,10,5,15,1,2,1,3]
+boss1=[7000,281,236,248,236];原boss1=[7000,281,236,248,236]
+boss2=[8000,281,236,248,236];原boss2=[8000,281,236,248,236]
+boss3=[9000,281,236,248,236];原boss3=[9000,281,236,248,236]
+boss4=[10000,281,236,248,236];原boss4=[10000,281,236,248,236]
+boss5=[20000,281,236,248,236];原boss5=[20000,281,236,248,236]
+boss6=[65000,281,236,248,236];原boss6=[65000,281,236,248,236]
+哈莫雷特pp=[10,15,15,5,3,4,1,1];原哈莫雷特pp=[10,15,15,5,3,4,1,1]
+羅特利斯pp=[20,15,25,5,0,3,0,2];原羅特利斯pp=[20,15,25,5,0,3,0,2]
+魔焰猩猩pp=[10,10,5,15,3,1,1,2];原魔焰猩猩pp=[10,10,5,15,3,1,1,2]
+薩帕克pp=[20,20,5,30,4,3,2,3];原薩帕克pp=[20,20,5,30,4,3,2,3]
+卡利斯pp=[10,15,20,10,0,0,3,1];原卡利斯pp=[10,15,20,10,0,0,3,1]
+雷伊pp=[3,10,5,15,1,2,1,3];原雷伊pp=[3,10,5,15,1,2,1,3]
+boss1pp=[100000,100000,100000]
+boss2pp=[10,10,10]
+boss3pp=[10,10,10]
+boss4pp=[10,10,10]
+boss5pp=[100000,100000,100000]
+boss6pp=[100000,100000,100000]
+道具數量=[5,5,15,15,5]
+
 ########################################################################################
 #print('歡迎來到賽爾號')
 #time.sleep(1)
